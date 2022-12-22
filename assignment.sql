9a81e9cb91f6349e3e33147bc6f774abeeb78f59
PaulSaviour <115450308+PaulSaviour@users.noreply.github.com>
2022-12-22 11:46:35 +0530

    Add files via upload

---
+++ assignment.sql
@@ -0,0 +1,112 @@
+create database assignment;
+use assignment;
+Create table salespeople
+( 
+
+snum int primary key, 
+
+sname varchar(10), 
+
+city varchar(10), 
+
+comm int(3)
+
+); 
+
+Insert into salespeople values(snum,name,city,comm);
+
+
+Create table customers 
+
+( 
+
+cnum int primary key, 
+
+cname varchar(10) NOT NULL, 
+
+city varchar(10), 
+
+rating int2(4), 
+
+snum int3(4) 
+
+); 
+Insert into customers values(cnum,cname,city,rating,snum); 
+
+Create table orders 
+
+( 
+
+onum int primary key, 
+
+amt int1(7), 
+
+odate date, 
+
+cnum int2(4), 
+
+snum int3(4) salespeoplesalespeople
+
+); 
+Insert into orders values(ONUM,AMT,ODATE,CNUM,SNUM);
+
+INSERT INTO ORDERS 
+
+VALUES(3001,18.69,'03-OCT-1990',2008, 1007); 
+
+ 
+
+INSERT INTO ORDERS 
+
+VALUES(3003,767.19,'03-OCT-1990',2001,1001); 
+
+ 
+
+INSERT INTO ORDERS 
+
+VALUES(3002,1900.10,'03-OCT-1990',2007,1004); 
+
+ 
+
+INSERT INTO ORDERS 
+
+VALUES(3005,5160.45,'03-OCT-1990',2003,1002); 
+
+ 
+
+INSERT INTO ORDERS 
+
+VALUES(3006,1098.16,'03-OCT-1990',2008,1007); 
+
+ 
+
+INSERT INTO ORDERS 
+
+VALUES(3009,1713.23,'04-OCT-1990',2002,1003); 
+
+ 
+
+INSERT INTO ORDERS 
+
+VALUES(3007,75.75,'04-OCT-1990',2004,1002); 
+
+ 
+
+INSERT INTO ORDERS 
+
+VALUES(3008,4723.00,'05-OCT-1990',2006,1001); 
+
+ 
+
+INSERT INTO ORDERS 
+
+VALUES(3010,309.95,'06-OCT-1990',2004,1002); 
+
+ 
+
+INSERT INTO ORDERS 
+
+VALUES(3011,9891.88,'06-OCT-1990',2006,1001); 
+
+
+
